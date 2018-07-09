2910f35ccf0181eff91d073d3e247871e09e1568
wangjiapeng <wangjiapeng@weizaojiao.cn>
2018-07-09 10:41:35 +0800

    easy array sort

---
+++ demo.php
@@ -0,0 +1,36 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+/**
+ * Created by PhpStorm.
+ * User: weizaojiao-wjp
+ * Date: 2018/7/6
+ * Time: 18:30
+ */
+include_once 'array_sort.php';
+
+$arr = array(
+    '3308'=>array(
+        'nickname'=>'白百2',
+        'age'=>'42'
+    ),
+    '3310'=>array(
+        'nickname'=>'张三4',
+        'age'=>'30'
+    ),
+    '3406'=>array(
+        'nickname'=>'王五3',
+        'age'=>'30'
+    ),
+    '3306'=>array(
+        'nickname'=>'安迪1',
+        'age'=>'26'
+    ),
+);
+
+$data['data'] = $arr;
+$data['key_name'] = 'nickname';
+$obj = new my_array_sort($data);
+
+$res = $obj->do_array();
+
+print_r($res);die;
